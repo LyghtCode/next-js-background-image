f882ab43da39a4e570de486b5fa75282a414b035
LyghtCode <xikago.musica@gmail.com>
2022-11-16 18:51:40 -0500

    merge local

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { useReducer } from "react";
+import {
+  DispatchPlayerContext,
+  PlayerContext,
+  playerInitialState,
+  playerReducer,
+} from "decent-audio-player";
+import "@fontsource/genos";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [state, dispatch] = useReducer(playerReducer, playerInitialState);
+
+  return (
+    
+    <PlayerContext.Provider value={state}>
+      <DispatchPlayerContext.Provider value={dispatch}>
+        <Component {...pageProps} />
+      </DispatchPlayerContext.Provider>
+    </PlayerContext.Provider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+import { Text, Navbar, Link,  Container, Row, Card, Col, Image } from '@nextui-org/react';
+import { AudioPlayer } from "decent-audio-player";
+
 
 
 export default function Home() {
@@ -10,20 +12,108 @@ export default function Home() {
         width: "100vw",
         height: "100vh"
       }}>
-        <Image 
-          src="/bg.webp"
-          alt="Mountains with snow"
+        <Image
+          //Main Background
+          src="/sound.gif"
+          alt="bg"
           layout="fill"
           objectFit='cover'
         />
+        <Navbar isBordered variant="floating">
+            {/* Artist Branding */}
+            <Navbar.Brand>
+              <Image src="/decent.png" width={60} height={60} alt="logo"/>
+            </Navbar.Brand>
+            {/* Socials/Dropdown Menu */}
+            <Navbar.Collapse>
+              <Navbar.CollapseItem>
+                <Link
+                  color="primary"
+                  css={{
+                    minWidth: "66%",
+                    fontFamily: "Genos",
+                    fontSize: "$3xl"
+                  }}
+                  href={"https://twitter.com/TainoMusica"}
+                >
+                  Twitter
+                </Link>
+              </Navbar.CollapseItem>
+              <Navbar.CollapseItem>
+              <Link
+                  color="primary"
+                  css={{
+                    minWidth: "66%",
+                    fontFamily: "Genos",
+                    fontSize: "$3xl"
+                  }}
+                  href={"https://www.instagram.com/Taino.Musica/"}
+                >
+                  Instagram
+                </Link>
+              </Navbar.CollapseItem>
+      
+            </Navbar.Collapse>
+
+            <Navbar.Toggle aria-label="toggle navigation" />
+
+          </Navbar>
+      {/* Decent Player component with background */}
+      <Container justify="center" sm="true" >
+        <Row justify='center'>
+        <Card variant='shadow' css={{ w: "380px", h: "600px", fontFamily: "Genos" }}>
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+            <Col>
+                {/* Artist name goes here */}
+                <Text size={24} weight="bold" transform="uppercase" color="#ffffff">
+                    Genesis
+                </Text>
+                {/* Track name goes here */}
+                <Text h3 color="white">
+                    Taino
+                </Text>
+            </Col>
+        </Card.Header>
+        <Card.Body css={{ p: 0 }}>
+            <Card.Image
+                // NFT Image or animation here
+                src="/sound.gif"
+                width="100%"
+                height="100%"
+                objectFit="cover"
+                alt="Card example background"
+            />
+        </Card.Body>
+        <Card.Footer
+            isBlurred
+            css={{
+                position: "absolute",
+                bgBlur: "#ffffff66",
+                borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                bottom: 0,
+                zIndex: 1,
+            }}
+        >
+            <Row>
+                <Col>
+                    <AudioPlayer
+                        size={60}
+                        audioSrc="/genesis.wav"
+                        callbackAfterPlay={console.log}
+                        active
+                    ></AudioPlayer>
+                </Col>
+                <Col>
+                    <Row>
+                        <Image src="/decent.png" width={60} height={60} alt="logo" />
+                    </Row>
+                </Col>
+            </Row>
+        </Card.Footer>
+    </Card>
+      </Row>
+      </Container>
       </div>
-      <h1 style={{
-        paddingTop: "30vh",
-        fontFamily: "monospace",
-        fontSize: "3.5rem",
-        fontWeight: "bold",
-        textAlign: "center"
-      }}>Next.js Background Image Tutorial</h1>
     </>
   )
 }
